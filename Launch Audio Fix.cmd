7edd46e8078b94adc8a6cbfb7259413a591a8825
Mitchell Thomas <50987548+MoltenZaz@users.noreply.github.com>
2020-06-20 03:50:00 +1000

    Delete Launch Audio Fix.cmd

--- Launch Audio Fix.cmd
+++
@@ -1,2 +0,0 @@
-F:\Documents\Nircmd\nircmd.exe exec hide "F:\Documents\Audio Fix Script\Audio Fix.cmd"
-:: this launches the audio fix script while hiding the command prompt it will need to be adapted to your setup
